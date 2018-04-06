90d3e62039aa66f35b309df8ae066d900a2e5f4e
Kyla Stearns <kdstearns@Kylas-MacBook-Air.local>
2018-04-05 20:03:22 -0500

    update blog.rb

--- kyla_stearns/Rails/blogs/app/models/blog.rb
+++ kyla_stearns/Rails/blogs/app/models/blog.rb
@@ -1,8 +1,4 @@
 class Blog < ActiveRecord::Base
-	has_many :owners
-	has_many :users, through: :owners
-	has_many :posts, required: true
-
 	validates :name, :description, presence: true
 	validates :name, length: { minimum: 5 }
 	validates :description, length: { minimum: 15 }
