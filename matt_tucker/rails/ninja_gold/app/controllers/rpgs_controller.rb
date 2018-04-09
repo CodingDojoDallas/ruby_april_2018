bcac1f49e855076a84aa7bec570aa421dbf43b95
Matt Tucker <mtuck06@gmail.com>
2018-04-09 16:42:45 -0500

    further clean up on ninja_gold

--- matt_tucker/rails/ninja_gold/app/controllers/rpgs_controller.rb
+++ matt_tucker/rails/ninja_gold/app/controllers/rpgs_controller.rb
@@ -1,43 +1,61 @@
 class RpgsController < ApplicationController
-	AMOUNTS = {
-		farm: rand(10..20),
-		cave: rand(5..10),
-		house: rand(2..5),
-		casino: rand(-50..50)
-	}
-
+	
 	def index
-		session[:gold] ||= 0
-		session[:activities] ||= []
+		set_session_defaults
 	end
 
 	def gamble
 		building = params[:building]
 
-		gold = AMOUNTS[building.to_sym]
+		gold = calculate_gold building
 		
 		activity = build_activity building, gold
-		
-		session[:gold] += gold
 
-		session[:activities].unshift activity
+		update_session gold, activity
 
 		redirect_to :back
 	end
 
-	private 
+	private
+		def calculate_gold building
+			amounts = {
+				farm: [10, 20],
+				cave: [5, 10],
+				house: [2, 5],
+				casino: [-50, 50]
+			}
+
+			values = amounts[building.to_sym]
+			
+			rand(values[0]..values[1])
+		end
+
 		def build_activity building, gold
 			activity = {}
+			time = Time.now.in_time_zone('Central Time (US & Canada)').strftime('%b %d, %Y, %l:%M')
 
 			if gold >= 0
 				activity[:color] = 'green'
-				activity[:message] = "Earned #{gold} golds from the #{building} #{Time.now}"
+				activity[:message] = "Earned #{gold} golds from the #{building} #{time}"
 			else
 				activity[:color] = 'red'
-				activity[:message] = "Entered a casino and lost #{gold} golds... Ouch... #{Time.now}"
+				activity[:message] = "Entered a casino and lost #{gold} golds... Ouch... #{time}"
 			end
 
 			return activity
+		end
+
+		def set_session_defaults
+			session[:gold] ||= 0
+			session[:activities] ||= []
+		end
+
+		def update_session gold, activity
+			session[:gold] += gold
+
+			session[:activities].unshift activity
+
+			return
 		end
 
 end
