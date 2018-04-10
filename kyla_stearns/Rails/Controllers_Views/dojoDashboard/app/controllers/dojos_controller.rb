718e8ede8083b5a4d0a48f95fc0abe6bd9105626
Kyla Stearns <kdstearns@Kylas-MacBook-Air.local>
2018-04-10 06:59:51 -0500

    add dojo dashboard

--- kyla_stearns/Rails/Controllers_Views/dojoDashboard/app/controllers/dojos_controller.rb
+++ kyla_stearns/Rails/Controllers_Views/dojoDashboard/app/controllers/dojos_controller.rb
@@ -17,19 +17,28 @@ class DojosController < ApplicationController
 	def create
     # form submission, add 1 to session and create user, then direct to results page
     	new_dojo = Dojo.create(dojo_params)
-    	if new_dojo.valid?
-	        flash[:success] = "Thanks for adding a new dojo!"
-	        return redirect_to '/dojos'
-    	end
+    	# if successful
+	    # flash[:success] = "Thanks for adding a new dojo!"
+	    return redirect_to "/dojos" if new_dojo.valid?
+	    # much longer way of saying the same thing
+    	# if new_dojo.valid?
+	    #     # flash[:success] = "Thanks for adding a new dojo!"
+	    #     return redirect_to '/dojos'
+    	# end
 
     	# Otherwise there were errors
     	flash[:errors] = new_dojo.errors.full_messages
-    	return redirect_to '/dojos/new'
+    	return redirect_to "/dojos/new"
+    	# EASY WAY TO REDIRECT BACK TO THE PAGE THEY WERE ON && flash is built in so just give it the key
+    	# THIS IS NOT WORKING... REDIRECTS BUT DOES NOT OUTPUT ERRORS
+    	# return redirect_to :back, errors: new_dojo.errors.full_messages
 	end
 
 	def show
 		# will automatically go into views/users and look for 'show' file to render
 		@this_dojo = Dojo.find(params[:id])
+		@all_students = Student.where(dojo: params[:id])
+		p @all_students
 	end
 
 	def edit
@@ -38,13 +47,26 @@ class DojosController < ApplicationController
 	end
 
 	def update
-		Dojo.update(params[:id], dojo_params)
-		redirect_to '/dojos'
+		update_dojo = Dojo.update(params[:id], dojo_params)
+		# successful
+		# return redirect_to '/dojos' if update_dojo.valid?
+		# much longer way of saying the same thing
+		if update_dojo.valid?
+			flash[:success] = "Thanks for updating dojo!"
+			return redirect_to "/dojos"
+		end
+
+		# Otherwise there were errors
+		flash[:errors] = update_dojo.errors.full_messages
+		redirect_to :back
+    	# EASY WAY TO REDIRECT BACK TO THE PAGE THEY WERE ON && flash is built in so just give it the key
+		# THIS IS NOT WORKING... REDIRECTS BUT DOES NOT OUTPUT ERRORS
+		# redirect_to :back, errors: update_dojo.errors.full_messages
 	end
 
 	def delete
 		Dojo.find(params[:id]).destroy
-		redirect_to '/dojos'
+		redirect_to "/dojos"
 	end
 
 	private
--- kyla_stearns/Rails/Controllers_Views/dojoDashboard/db/schema.rb
+++ kyla_stearns/Rails/Controllers_Views/dojoDashboard/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409004753) do
+ActiveRecord::Schema.define(version: 20180409131633) do
 
   create_table "dojos", force: :cascade do |t|
     t.string   "branch"
@@ -21,5 +21,16 @@ ActiveRecord::Schema.define(version: 20180409004753) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.integer  "dojo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "students", ["dojo_id"], name: "index_students_on_dojo_id"
 
 end
